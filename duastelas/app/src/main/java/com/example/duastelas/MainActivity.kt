54e2aa9b5ddb13fd07da5d30829c389087d844b0
u22597 <cc22597@g.unicamp.br>
2023-03-13 20:31:19 -0300

    colocação do botão no xml

--- duastelas/app/src/main/java/com/example/duastelas/MainActivity.kt
+++ duastelas/app/src/main/java/com/example/duastelas/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnGoToTela2 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoToTela2 = findViewById(R.id.btnGoToTela1)
+        btnGoToTela2.setOnClickListener{ it: View
+            val intent = Intent(this, tela2::class.java)
+            startActivity(intent)
     }
 }
