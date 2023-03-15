7b2930f9dd2f439268813b8c58b862ac8aed1c3b
u22597 <cc22597@g.unicamp.br>
2023-03-15 19:32:44 -0300

    Inserindo retorno para a tela1

--- duastelas/app/src/main/java/com/example/duastelas/Tela2.kt
+++ duastelas/app/src/main/java/com/example/duastelas/Tela2.kt
@@ -2,10 +2,16 @@ package com.example.duastelas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Tela2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+
+        val btnVoltar = findViewById(R.id.btnVoltar) as Button
+        btnVoltar.setOnClickListener{
+            this.finish()
+        }
     }
 }
